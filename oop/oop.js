9243679da387db5701524d56ec6ea9109c47804d
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2021-04-16 11:12:23 +0100

    Added oop practice

--- oop/oop.js
+++ oop/oop.js
@@ -113,7 +113,7 @@ Here is an example of a constructor:
  */
 function Bird() {
     this.name = "Albert";
-    this.color = blue;
+    this.color = "Blue";
     this.numLegs = 2;
 }
 
@@ -155,3 +155,68 @@ blueBird.name;
 
 Use the Dog constructor from the last lesson to create a new instance of Dog, assigning it to a variable hound.*/
 let hound = new Dog4();
+console.log(hound);
+
+/**         Extend Constructors to Receive Arguments
+The Bird and Dog constructors from the last challenge worked well. However, notice that all Birds that are created with the Bird constructor are automatically named Albert, are blue in color, and have two legs. What if you want birds with different values for name and color? It's possible to change the properties of each bird manually but that would be a lot of work:
+
+ */
+
+let swan = new Bird();
+swan.name = "Carlos";
+swan.color = "White";
+console.log(swan);
+
+/**Suppose you were writing a program to keep track of hundreds or even thousands of different birds in an aviary. It would take a lot of time to create all the birds, then change the properties to different values for every one. To more easily create different Bird objects, you can design your Bird constructor to accept parameters: */
+
+function Birds(name, color) {
+    this.name = name;
+    this.color = color;
+    this.numLegs = 2;
+}
+
+/**Then pass in the values as arguments to define each unique bird into the Bird constructor: let cardinal = new Bird("Bruce", "red"); This gives a new instance of Bird with name and color properties set to Bruce and red, respectively. The numLegs property is still set to 2. The cardinal has these properties: */
+let cardinal = new Birds("Bruce", "Red")
+console.log(cardinal);
+
+/**The constructor is more flexible. It's now possible to define the properties for each Bird at the time it is created, which is one way that JavaScript constructors are so useful. They group objects together based on shared characteristics and behavior and define a blueprint that automates their creation.
+ * 
+ * Create another Dog constructor. This time, set it up to take the parameters name and color, and have the property numLegs fixed at 4. Then create a new Dog saved in a variable terrier. Pass it two strings as arguments for the name and color properties.
+ */
+function Adog(name, color) {
+    this.name = name;
+    this.color = color;
+    this.numLegs = 4;
+}
+let terrier = new Adog("Screamer", "Grey");
+console.log(terrier);
+
+/**Verify an Object's Constructor with instanceof
+Anytime a constructor function creates a new object, that object is said to be an instance of its constructor. JavaScript gives a convenient way to verify this with the instanceof operator. instanceof allows you to compare an object to a constructor, returning true or false based on whether or not that object was created with the constructor. Here's an example: */
+
+let Bird1 = function(name, color){
+    this.name = name;
+    this.color = color;
+    this.numLegs = 2;
+}
+let crow = new Bird1("Tiker", "Silver");
+console.log(crow instanceof Bird1); 
+/**This instanceof method would return true.
+
+If an object is created without using a constructor, instanceof will verify that it is not an instance of that constructor: */
+
+let bird = {
+    name: "zizy",
+    color: "Multi",
+    numLegs: 2
+};
+console.log(bird instanceof Bird1 );
+/**This instanceof method would return false. 
+ * 
+ * Create a new instance of the House constructor, calling it myHouse and passing a number of bedrooms. Then, use instanceof to verify that it is an instance of House.
+*/
+function House(numBedrooms) {
+    this.numBedrooms = numBedrooms
+}
+let myHouse = new House(5);
+console.log(myHouse instanceof House);
