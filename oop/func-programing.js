93bbc49b1fe19fefc10a4aa84f97a9297f8d775b
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2021-04-29 00:37:51 +0100

    Added additional practices

--- oop/func-programing.js
+++ oop/func-programing.js
@@ -172,8 +172,90 @@ Change Window.prototype.tabClose so that it removes the correct tab. */
         this.tabs = this.tabs.concat(otherWindows.tabs);
         return this;
     };
+// When you open new tab at the end.
+Window.prototype.tabOpen = function (tab) {
+    this.tabs.push("new tab"); // let open new tab for now
 
-    let workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']);
+      return this;
+};
+
+// when you close a tab
+
+    Window.prototype.tabClose = function (index) {
+        // Only change code below this line
+
+        var tabsBeforeIndex = this.tabs.splice(0, index);
+        // Get the tabs before the tab
+
+        var tabsAfterIndex = this.tabs.splice(1)
+        // Get the tabs after the tab
+
+        this.tabs = tabsBeforeIndex.concat(tabsAfterIndex)
+        // Join them together
+
+        // Only change code above this line
+
+        return this;
+
+
+    };
+
+    //lets create three browser Window
+    var workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']);
+    // Your mailbox, drive, and other work sites
+
+    var socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']);
+    //social Sites;
+
+    var videoWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']);
+    // Entertainment sites
+
+    // Now perform the tab opening, closing, and other operations
+    var finalTabs = socialWindow
+    .tabOpen() // Open a new tab for cat memes
+    .join(videoWindow.tabClose(2))
+    // Close third tab in video window, and join
+    .join(workWindow.tabClose(1).tabOpen());
+    console.log(finalTabs.tabs);
+
+/**Avoid Mutations and Side Effects Using Functional Programming
+ * 
+ * If you haven't already figured it out, the issue in the previous 
+ * challenge was with the splice call in the tabClose() function. 
+ * Unfortunately, splice changes the original array it is called on, 
+ * so the second call to it used a modified array, and gave unexpected results.
+ * 
+ * This is a small example of a much larger pattern - you call 
+ * a function on a variable, array, or an object, and the function changes the variable or something in the object.
+ * 
+ * One of the core principles of functional programming is to not change things. 
+ * Changes lead to bugs. It's easier to prevent bugs knowing that your 
+ * functions don't change anything
+ * including the function arguments or any global variable.
+ * 
+ * The previous example didn't have any complicated 
+ * operations but the splice method changed the original array, 
+ * and resulted in a bug.
+ * 
+ * Recall that in functional programming, changing or altering things 
+ * is called mutation, and the outcome is called a side effect. 
+ * A function, ideally, should be a pure function, meaning that it 
+ * does not cause any side effects.
+ * 
+ * Let's try to master this discipline and not alter 
+ * any variable or object in our code. 
+ * 
+ * Fill in the code for the function incrementer so it returns 
+ * the value of the global variable fixedValue increased by one.*/    
+
+let fixedValue = 4;
+
+function incrementer() {
+   return fixedValue + 1;
+}
+console.log(incrementer());
+console.log(fixedValue);
+
 
 
 
