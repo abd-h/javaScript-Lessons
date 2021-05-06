77898b16e757ae28ff04a097ce301dce2fd188dc
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2021-05-07 00:58:01 +0100

    Added map() methode

--- oop/func-programing.js
+++ oop/func-programing.js
@@ -454,9 +454,9 @@
     const newUsers = newUser
     .map(function (name, ages) {
       return  name.name
-        ages[0];
+      
         
-    },0)
+    })
     console.log(newUsers);
 
     /**The console would display the value [ 'John', 'Amy', 'camperCat' ].
@@ -612,9 +612,36 @@
     }));
       
     console.log(newArr1);
-    let item = watchList.
+    let item = watchList.map(film => ({
+        movie: film.Title,
+        year: film["Year"]
+        
+    }))
+    console.log(item);
+//     returns 0: {movie: "Inception", year: "2010"}
+// 1: {movie: "Interstellar", year: "2014"}
+// 2: {movie: "The Dark Knight", year: "2008"}
+// 3: {movie: "Batman Begins", year: "2005"}
+// 4: {movie: "Avatar", year: "2009"}
     
+/**Implement map on a Prototype
+As you have seen from applying Array.prototype.map(), 
+or simply map() earlier, the map method returns 
+an array of the same length as the one it was called on. 
+It also doesn't alter the original array, as long as its callback function doesn't.
 
+In other words, map is a pure function, 
+and its output depends solely on its inputs. 
+Plus, it takes another function as its argument.
+
+You might learn a lot about the map method if you implement your own version of it. 
+It is recommended you use a for loop or Array.prototype.forEach().
+
+Write your own Array.prototype.myMap(), which should behave 
+exactly like Array.prototype.map(). 
+You should not use the built-in map method. 
+The Array instance can be accessed in the myMap method using this.
+ */
 
 
 
