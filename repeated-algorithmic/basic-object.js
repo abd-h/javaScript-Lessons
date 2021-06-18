91d120542d8d5b6d46e5579c5e4d14e9097a0c9b
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2021-06-19 00:12:40 +0100

    Added basic object practice

--- repeated-algorithmic/basic-object.js
+++ repeated-algorithmic/basic-object.js
@@ -386,3 +386,10 @@ function phoneticLookup(val) {
 }
 console.log(phoneticLookup("charlie"));
 
+/*      Testing Objects for Properties
+
+Sometimes it is useful to check if the property of a given object exists or not. 
+We can use the .hasOwnProperty(propname) method of objects to determine if that object has the given property name. .hasOwnProperty() returns true or false if the property is found or not.
+
+*/
+
