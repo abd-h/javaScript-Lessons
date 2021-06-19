29aacd01fd760322d99e4e4b264df4840064aa72
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2021-06-20 00:48:49 +0100

    practicing basic js loops

--- repeated-algorithmic/basic-loops.js
+++ repeated-algorithmic/basic-loops.js
@@ -13,3 +13,18 @@ while(i < 5) {
     i++;
 }
 console.log(ourArray);
+
+/*In the code example above, the while loop will execute 5 times 
+and append the numbers 0 through 4 to ourArray.
+
+Let's try getting a while loop to work by pushing values to an array.
+
+Add the numbers 5 through 0 (inclusive) in descending order to myArray 
+using a while loop.*/
+
+let myArray = [];
+let i = 0;
+while (i < 5) {
+    myArray.unshift(i);
+    i++
+}
