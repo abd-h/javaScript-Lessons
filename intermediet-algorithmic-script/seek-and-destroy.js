ba5c47beb4557d3c0ff946d35b9f5c3f024ac3b4
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2021-07-31 00:20:22 +0100

    Added more practice of intermidietalgorithm

--- intermediet-algorithmic-script/seek-and-destroy.js
+++ intermediet-algorithmic-script/seek-and-destroy.js
@@ -87,3 +87,31 @@ function destroyer(arr){
    return sameArray;
 }
 console.log(destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3));
+
+
+/*
+ Wherefore art thou
+ Make a function that looks through an array of objects (first argument) and returns an array of all objects 
+ that have matching name and value pairs (second argument). Each name and value pair of the source object has 
+ to be present in the object from the collection if it is to be included in the returned array.
+
+For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }
+, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return 
+the third object from the array (the first argument), because it contains the name and its value, that was 
+passed on as the second argument.
+
+ function whatIsInAName(collection, source) {
+  var arr = [];
+  // Only change code below this line
+
+
+  // Only change code above this line
+  return arr;
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", 
+last: "Capulet" }], { last: "Capulet" });*/
+
+function whatIsInAName(collection, source){
+  
+}
