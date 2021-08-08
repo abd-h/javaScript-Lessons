4b391b9c2eb161eb4d22f666482d6bc062c5daab
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2021-08-08 01:38:48 +0100

    Added search and unfinished search and replace challenge

--- intermediet-algorithmic-script/part-two.js
+++ intermediet-algorithmic-script/part-two.js
@@ -76,14 +76,89 @@ function translatePigLatin(str){
     let regex = /^[^aeiou]+/g,
         vowels = /^[aeiou]/gi;
     const constant = str.match(regex);
-    if (constant === null) {
-        str = str.concat("way");
+    if (vowels.test(constant)) {
+        return str.concat("way");
     }
     else {
-        str = str.replace(regex, "").concat(constant).concat("ay");
+        return str.replace(regex, "").concat(constant).concat("ay");
     }
-          return str  
 }
 console.log(translatePigLatin("paragraphs"));
 //translatePigLatin("paragraphs") should return the string aragraphspay.
+
+/*
+Search and Replace
+Perform a search and replace on the sentence using the arguments provided 
+and return the new sentence.
+
+First argument is the sentence to perform the search and replace on.
+
+Second argument is the word that you will be replacing (before).
+
+Third argument is what you will be replacing the second argument 
+with (after).
+
+Note: Preserve the case of the first character in the original word 
+when you are replacing it. For example if you mean to replace the 
+word Book with the word dog, it should be replaced as Dog
+
+myReplace("Let us go to the store", "store", "mall") should return 
+the string Let us go to the mall.
+
+myReplace("He is Sleeping on the couch", "Sleeping", "sitting") 
+should return the string He is Sitting on the couch.
+
+myReplace("I think we should look up there", "up", "Down") 
+should return the string I think 
+we should look down there.
+
+myReplace("This has a spellngi error", "spellngi", "spelling") 
+should return the string This has a spelling error.
+
+myReplace("His name is Tom", "Tom", "john") should return the string His name is John.
+
+myReplace("Let us get back to more Coding", "Coding", "algorithms") 
+should return the string Let us get back to more Algorithms.
+...............................................
+
+function myReplace(str, before, after) {
+  return str;
+}
+
+myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");*/
+
+let jarr = "jumped";//before
+let r = "A quick brown fox jumped over the lazy dog"; // str
+let j = "jump"//after
+let s = r.split(" ").slice();//  first variable used to copy str
+
+
+console.log(s);
+let regex1 = s.indexOf(jarr); // used to find index of before
+let f = s.splice(regex1);
+console.log(f);
+let nf = f.shift();
+f.unshift(j)
+console.log(f);
+console.log(s.concat(f));
+ let results = s.concat(f).join(" ");
+ console.log(results);
+
+function myReplace(str, before, after) {
+    let copyStr = str.split(" ").slice();
+    let find = copyStr.indexOf(before);
+    let firstCut = copyStr.splice(find); 
+    firstCut.shift();
+    firstCut.unshift(after);
+    return copyStr.concat(firstCut).join(" ");
+}
+console.log(myReplace("I think we should look up there", "up", "Down"));
+
+
+
+
+
+
+
+
 
