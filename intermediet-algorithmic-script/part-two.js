fb08d36ea8ae1e5a2682d2ae794d95bebd80f771
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2021-08-12 14:24:03 +0100

    Added DNA Pair Challenge

--- intermediet-algorithmic-script/part-two.js
+++ intermediet-algorithmic-script/part-two.js
@@ -173,7 +173,51 @@ myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");*/
        return s.concat(a).join(" ")
     }
 }
+//...........................................................................................
 
+
+/*
+
+DNA Pairing
+The DNA strand is missing the pairing element. Take each character, get its pair, 
+and return the results as a 2d array.
+
+Base pairs are a pair of AT and CG. Match the missing element to the provided character.
+
+Return the provided character as the first element in each array.
+
+For example, for the input GCG, return [["G", "C"], ["C","G"], ["G", "C"]]
+
+The character and its pair are paired up in an array, and all the arrays are grouped 
+into one encapsulating array.
+
+pairElement("ATCGA") should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]].
+
+pairElement("TTGAG") should return [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]].
+
+pairElement("CTCTA") should return [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]].
+
+function pairElement(str) {
+  return str;
+}
+
+pairElement("GCG");
+
+*/
+
+
+function pairElement(str){
+    let string = str.split("").slice();
+    let pairing1 = [["A", "T"], ["T", "A"], ["G", "C"], ["C", "G"]];
+    let result = [];
+    string.filter((a) => (a.indexOf("A") !== -1)? result.push(pairing1[0])
+                                  :(a.indexOf("T") !== -1)? result.push(pairing1[1])
+                                  :(a.indexOf("G") !== -1)? result.push(pairing1[2])
+                                  :(a.indexOf("C") !== -1)? result.push(pairing1[3])
+                                  : result);
+    return result;
+}    
+console.log(pairElement("ATCGA"));
 
 
 
